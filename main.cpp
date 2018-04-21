6970e2d6c5001b6b1f3d99c2a8c97bb159f98587
Marta Skreta <skretam@mcmaster.ca>
2018-04-20 21:53:34 -0400

    fixed bug in rand func

--- main.cpp
+++ main.cpp
@@ -114,24 +114,25 @@ int main(int argc, char* argv[]){
 
  	/******** Allocate particle weight, position, and velocity to array ********/
  		for(i = 0; i < numParticlesTotal; i++){
+ 			srand (time(NULL));
  			if(i < numParticlesLight){
- 				w[i] = rand % (massLightMin-massLightMax+1) + massLightMin;
+ 				w[i] = rand() % (massLightMin-massLightMax+1) + massLightMin;
  				s_x[i] = drand48()*imageWidth;
  				s_y[i] = drand48()*imageHeight;
- 				v_x[i] = rand % (velocityLightMax-velocityLightMin+1) + velocityLightMin;
- 				v_y[i] = rand % (velocityLightMax-velocityLightMin+1) + velocityLightMin;
+ 				v_x[i] = rand() % (velocityLightMax-velocityLightMin+1) + velocityLightMin;
+ 				v_y[i] = rand() % (velocityLightMax-velocityLightMin+1) + velocityLightMin;
  			} else if(i >= numParticlesLight && i < (numParticlesLight+numParticlesMedium)){
- 				w[i] = w[i] = rand % (massMediumMin-massMediumMax+1) + massMediumMin;
+ 				w[i] = w[i] = rand() % (massMediumMin-massMediumMax+1) + massMediumMin;
   				s_x[i] = drand48()*imageWidth;
  				s_y[i] = drand48()*imageHeight;
- 				v_x[i] = rand % (velocityMediumMax-velocityMediumMin+1) + velocityMediumMax;
- 				v_y[i] = rand % (velocityMediumMax-velocityMediumMin+1) + velocityMediumMax;
+ 				v_x[i] = rand() % (velocityMediumMax-velocityMediumMin+1) + velocityMediumMax;
+ 				v_y[i] = rand() % (velocityMediumMax-velocityMediumMin+1) + velocityMediumMax;
  			} else{
- 				w[i] = rand % (massHeavyMin-massHeavyMax+1) + massHeavyMin;
+ 				w[i] = rand() % (massHeavyMin-massHeavyMax+1) + massHeavyMin;
  				s_x[i] = drand48()*imageWidth;
  				s_y[i] = drand48()*imageHeight;
- 				v_x[i] = rand % (velocityHeavyMin-velocityHeavyMax+1) + velocityHeavyMin;
- 				v_y[i] = rand % (velocityHeavyMin-velocityHeavyMax+1) + velocityHeavyMin;
+ 				v_x[i] = rand() % (velocityHeavyMin-velocityHeavyMax+1) + velocityHeavyMin;
+ 				v_y[i] = rand() % (velocityHeavyMin-velocityHeavyMax+1) + velocityHeavyMin;
  			}
  		}
 
