a001108e0863e5ab8f8879ebbe8a306638f42ba6
Marta Skreta <skretam@mcmaster.ca>
2018-04-24 23:26:44 -0400

    print function for number of omp threads

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char* argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-	
+	printf("Number of threads: %d\n",omp_get_num_threads());
 
 	//variables
 	int numParticlesLight = std::stoi(argv[1]);
